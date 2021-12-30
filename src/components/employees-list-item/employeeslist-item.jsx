42281d853c08fbecb5b52d5db61b338645d4ff67
Evgen <e.kizilov95@gmail.com>
2021-12-30 20:03:46 +0200

    add functional for list

--- src/components/employees-list-item/employeeslist-item.jsx
+++ src/components/employees-list-item/employeeslist-item.jsx
@@ -1,22 +1,52 @@
+import { Component } from 'react';
 import './employeeslist-item.css';
 
-export default function EmployeeslistItem({name, salary, cookies}) {
-    return (
-        <li className={cookies ? "list-group-item increase  d-flex justify-content-between":"list-group-item d-flex justify-content-between"}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
+export default class EmployeeslistItem extends Component {
+    constructor(props) {
+       super(props);
+       this.state = {
+        isIncrease: true,
+        isRaise: false,
+       }
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    increase = () => {   
+        this.setState(({isIncrease}) => ({isIncrease: !isIncrease}));
+    }
+
+    raise =() => {
+        this.setState(({isRaise}) => ({isRaise: !isRaise}));
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        let classNames = 'list-group-item d-flex justify-content-between';
+
+        if(!this.state.isIncrease) {
+            classNames += ' increase';
+        }
+
+        if(this.state.isRaise) {
+            classNames += ' like';
+        }
+
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={this.raise}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm" onClick={this.increase} >
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button"
+                            className="btn-trash btn-sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,9 +1,8 @@
-
 import EmployeeslistItem from '../employees-list-item/employeeslist-item';
 
 export default function EmployeesList({employees}) {
-    const elements = employees.map(item => {
-        return <EmployeeslistItem {...item}/>
+    const elements = employees.map((item, i) => {
+        return <EmployeeslistItem key={i} {...item}/>
     });
 
     return (
