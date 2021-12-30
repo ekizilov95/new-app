200f7ce8818505bbe72352fb085ee3fc280dbce0
Evgen <e.kizilov95@gmail.com>
2021-12-30 20:09:28 +0200

    tabulation

--- src/components/employees-add-form/employees-add-form.jsx
+++ src/components/employees-add-form/employees-add-form.jsx
@@ -17,25 +17,25 @@ export default class EmployeesAddForm extends Component {
     render() {
         return (
             <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?"
-                    name="name"
-                    onChange={({target}) => this.onValueChange(target)}
-                />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" 
-                    name="salary"
-                    onChange={({target}) => this.onValueChange(target)}
-                />
-                <button type="submit"
-                    className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"
+                        name="name"
+                        onChange={({target}) => this.onValueChange(target)}
+                    />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $?" 
+                        name="salary"
+                        onChange={({target}) => this.onValueChange(target)}
+                    />
+                    <button type="submit"
+                        className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
         )
     }
 }
